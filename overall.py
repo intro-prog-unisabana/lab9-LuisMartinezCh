d274a316aad29db0fc8938c97f9ff52f99d3efd1
Luis Martinez <luismach@unisabana.edu.co>
2026-05-04 20:02:01 -0500

    lab9 try 4

--- overall.py
+++ overall.py
@@ -1,44 +1,59 @@
-# TODO: Import modules
+import car_utils
+from car import Car
+
+cars = {}
+
 def main():
-    cars = {}  # Dictionary to store cars with car_id as key and car objects as values
-
     while True:
-        print("\nMenu:")
-        print("1. Add a new car")
-        print("2. View all cars")
-        print("3. Drive a car")
-        print("4. Paint a car")
+        print("\n--- Car Management System ---")
+        print("1. Add Car")
+        print("2. View All Cars")
+        print("3. Drive Car")
+        print("4. Paint Car")
         print("5. Exit")
+        
+        choice = input("Select an option: ")
 
-        choice = input("Choose an option:\n")
+        if choice == "1":
+            # TODO: Agregar un auto nuevo
+            new_car = car_utils.create_car_from_input()
+            cars[new_car.car_id] = new_car
+            print(new_car)
+            print("Car added.")
 
-        if choice == '1':
-          """TODO: Call the appropriate function from utils.py to create 
-          the car, add it to the dictionary, and print the car."""
+        elif choice == "2":
+            # TODO: Ver todos los autos
+            car_utils.display_cars(cars)
 
+        elif choice == "3":
+            # TODO: Conducir un auto
+            car_id = input("Enter car ID:\n")
+            miles = float(input("Enter miles to drive:\n"))
+            
+            if car_id in cars:
+                cars[car_id].drive(miles)
+                print("Mileage updated.")
+                print(cars[car_id])
+            else:
+                print("Error: Car ID not found.")
 
-        elif choice == '2':
-          """TODO: Call the appropriate function from utils.py to display
-          all the cars in the cars dictionary."""
+        elif choice == "4":
+            # TODO: Pintar un auto
+            car_id = input("Enter car ID:\n")
+            new_color = input("Enter new color:\n")
+            
+            if car_id in cars:
+                cars[car_id].change_color(new_color)
+                print("Color updated.")
+                print(cars[car_id])
+            else:
+                print("Error: Car ID not found.")
 
-        elif choice == '3':
-          car_id = input("Enter the car ID to drive:\n")
-          miles = float(input("How many miles to drive?\n"))
-          """TODO: Look up the car in the dictionary, call the appropriate
-          class method to increase the mileage of the car, and print the car."""
-          
-        elif choice == '4':
-          car_id = input("Enter the car ID to paint:\n")
-          new_color = input("Enter the new color:\n")
-          """TODO: Look up the car in the dictionary, call the appropriate
-          class method to change the color of the car, and print the car."""
-
-        elif choice == '5':
+        elif choice == "5":
             print("Goodbye!")
             break
-
         else:
-            print("Invalid option. Please try again.")
+            print("Invalid option, try again.")
 
 if __name__ == "__main__":
     main()
