633e7ba54a5da47efd5aa690131d0a24ace2a60a
Luis Martinez <luismach@unisabana.edu.co>
2026-05-04 19:55:52 -0500

    lab9 try3

---
+++ practice.py
@@ -0,0 +1,17 @@
+from song import Song
+
+def print_songs(song_list):
+    """
+    Recibe una lista de objetos Song e imprime cada uno.
+    """
+    for song in song_list:
+        print(song)
+
+# Bloque de prueba
+if __name__ == "__main__":
+    songs = [
+        Song("tv off", "Kendrick Lamar", 3.7),
+        Song("Alright", "Kendrick Lamar", 3.5)
+    ]
+    
+    print_songs(songs)
